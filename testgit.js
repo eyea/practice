aca8aba07f32ed8d615ce58362dd9fca8d1af9ce
eyea_github <eyea@eyeseau.com>
2017-01-10 09:10:59 +0800

    morning

--- testgit.js
+++ testgit.js
@@ -4,4 +4,4 @@ this is the second commit test.
 this is the stage test .
 
 this is to check GIT change not file.  version 2  version 3
-this is to check GIT change not file .version 4
+this is to check GIT change not file .version 4 version 5
