19e6a524c28ee61a66641afb9599a12f0cabed4c
eyea <deyesea@gmail.com>
2017-03-05 21:59:12 +0800

    delete some useless files

--- testgit.js
+++
@@ -1,7 +0,0 @@
-this is the first commit test.
-this is the second commit test. 
-
-this is the stage test .
-
-this is to check GIT change not file.  version 2  version 3
-this is to check GIT change not file .version 4 version 5
